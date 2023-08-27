4b21ffba8d0cab728020e478ba5969d2679b6066
Kevin Yao <kevinyao2003@hotmail.com>
2023-08-27 16:58:38 +1200

    Revert "readded basic implementation of pagation"
    This reverts commit 0d50f8bfaaeb16298ffd41d506e56c124e70d53b.

--- games/browse/browse.py
+++ games/browse/browse.py
@@ -4,22 +4,17 @@ from games.browse import services
 browse_blueprint = Blueprint(
     'games_bp', __name__)
 
-PER_PAGE = 10
-
-browse_blueprint.route('/browse', defaults={'page': 1})
-@browse_blueprint.route('/browse/page/<int:page>')
-def browse_games(page):
-    offset = (page - 1) * PER_PAGE
-    games = services.get_games(offset, PER_PAGE, repo.repo_instance)
-    total_games = services.get_number_of_games(repo.repo_instance)
-    
+@browse_blueprint.route('/browse', methods=['GET'])
+def browse_games():
+    num_games = services.get_number_of_games(repo.repo_instance)
+    all_games = services.get_games(repo.repo_instance)
     return render_template(
         'browse/browse.html',
-        games=games,
-        page=page,
-        total_pages=(total_games + PER_PAGE - 1) // PER_PAGE
+        title=f"Browse Games | CS235 Game Library",
+        heading="Browse Games",
+        games=all_games,
+        num_games=num_games
     )
-
 
 @browse_blueprint.route('/search/<query>', methods=['GET'])
 def search_games(query):
